eddd98888f6c8c1e71e88dd8c7202f4c465d3ece
Francisco Joel Cevallos <joelcevallos363@gmail.com>
2022-10-13 21:29:27 -0500

    second commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import TaskListComponent from './components/container/task_list';
+import ComponenteA from './components/container/componenteA';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <TaskListComponent></TaskListComponent> */}
+        <ComponenteA></ComponenteA>
+      </header>
+    </div>
+  );
+}
+
+export default App;
