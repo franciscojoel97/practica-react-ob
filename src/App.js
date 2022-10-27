7e29aa71a9d27129efb56adf953b6a8e77eb8990
Francisco Joel Cevallos <joelcevallos363@gmail.com>
2022-10-26 21:13:23 -0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import TaskListComponent from './components/container/task_list';
 import ComponenteA from './components/container/componenteA';
 import Ejemplo1 from './hooks/ejemplo1';
+import Ejemplo2 from './hooks/ejemplo2';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         {/* <TaskListComponent></TaskListComponent> */}
         {/* <ComponenteA></ComponenteA> */}
         {/* Ejemplo de uso de hooks */}
-        <Ejemplo1></Ejemplo1>
+        {/* <Ejemplo1></Ejemplo1> */}
+        <Ejemplo2></Ejemplo2>
       </header>
     </div>
   );
