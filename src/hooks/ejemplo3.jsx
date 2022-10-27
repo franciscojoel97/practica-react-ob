44a4bbe4f2b12d7a538944c9d0794cce00af8144
Francisco Joel Cevallos <joelcevallos363@gmail.com>
2022-10-26 21:23:23 -0500

    uso del useContext

---
+++ src/hooks/ejemplo3.jsx
@@ -0,0 +1,37 @@
+/**
+ * ? Ejemplo Hooks:
+ * -> useState()
+ * -> useContext()
+*/
+import React, {useState, useContext} from 'react'
+
+/**
+ * 
+ * @returns Componente 1
+ * Dispone de un contexto que va a tener un valor 
+ * que recibe desde el padre
+*/
+
+const Componente1 = () => {
+
+    const state = React.createContext(null);
+
+    return (
+        <div>
+      
+        </div>
+    )
+}
+
+const Componente2 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
+
+
+
