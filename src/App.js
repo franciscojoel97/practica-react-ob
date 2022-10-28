4fda999c61c7072e350cdaece52a27ced6274ff2
Francisco Joel Cevallos <joelcevallos363@gmail.com>
2022-10-27 21:20:58 -0500

    useContext add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TaskListComponent from './components/container/task_list';
 import ComponenteA from './components/container/componenteA';
 import Ejemplo1 from './hooks/ejemplo1';
 import Ejemplo2 from './hooks/ejemplo2';
+import MiComponenteConContexto from './hooks/ejemplo3';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
         {/* <ComponenteA></ComponenteA> */}
         {/* Ejemplo de uso de hooks */}
         {/* <Ejemplo1></Ejemplo1> */}
-        <Ejemplo2></Ejemplo2>
+        {/* <Ejemplo2></Ejemplo2> */}
+        <MiComponenteConContexto></MiComponenteConContexto>
       </header>
     </div>
   );
