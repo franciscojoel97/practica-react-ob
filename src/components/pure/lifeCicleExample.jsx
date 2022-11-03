4cf44e16924ee9a5b9571395e85fffcc11a4c38a
Francisco Joel Cevallos <joelcevallos363@gmail.com>
2022-11-03 10:38:54 -0500

    Ejemplo de ciclo de vida

--- src/components/pure/lifeCicleExample.jsx
+++ src/components/pure/lifeCicleExample.jsx
@@ -7,7 +7,23 @@ import React, { Component } from "react";
 import PropTypes from 'prop-types';
 
 class LifeCycleExample extends Component {
+    
     constructor(props) {
         super(props);
+        console.log('CONSTRUCTOR: Cuando se instancia el componente');
     }
+
+    componentWillMount() {
+        console.log('WILLMOUNT: antes del montaje del componente');
+    }
+
+    componentDidMount() {
+        console.log('DIDMOUNT: justo antes de acabar el montaje del componente, antes de renderizarlo');
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log('WillReceiveProps: si va a recibir nuevas props');
+    }
+
+    
 }
