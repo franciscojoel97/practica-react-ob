d6e727d297dd7d271139576327f2ba5cf5b3bf60
Francisco Joel Cevallos <joelcevallos363@gmail.com>
2022-11-03 11:11:01 -0500

    Ejemplo didupdate componente funcional

---
+++ src/hooks/lifecycle/DidUpdate.jsx
@@ -0,0 +1,16 @@
+/**
+ * Ejemplo de uso de metodo de componentDidUpdate en componente clase
+ * y uso de hook en componente funcional
+*/
+
+import React, { Component } from "react";
+
+export class DidUpdate extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
