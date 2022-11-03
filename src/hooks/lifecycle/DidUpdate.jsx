2791a61f2decd47105b5a9ae00853b3c2c30e85c
Francisco Joel Cevallos <joelcevallos363@gmail.com>
2022-11-03 11:17:19 -0500

    Ejemplo didupdate componente funcional

--- src/hooks/lifecycle/DidUpdate.jsx
+++ src/hooks/lifecycle/DidUpdate.jsx
@@ -3,14 +3,32 @@
  * y uso de hook en componente funcional
 */
 
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 
 export class DidUpdate extends Component {
+
+    componentDidUpdate() {
+        console.log('Comportamiento cuando el componente recibe nuevos props o cambios en su estado ');
+    }
+
     render() {
         return (
             <div>
-                
+                <h1>DidUpdate</h1>
             </div>
         )
     }
+}
+
+export const DidupdateHook = () => {
+    
+    useEffect(() => {
+        console.log('Comportamiento cuando el componente recibe nuevos props o cambios en su estado ');
+    });
+
+    return (
+        <div>
+            <h1>DidUpdate</h1>
+        </div>
+    );
 }
