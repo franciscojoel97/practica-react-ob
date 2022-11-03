6fb5f5b9f714810b375080c56883944de7ad6172
Francisco Joel Cevallos <joelcevallos363@gmail.com>
2022-11-03 10:28:31 -0500

    Ejemplo de ciclo de vida

---
+++ src/components/pure/lifeCicleExample.jsx
@@ -0,0 +1,13 @@
+/**
+ * Ejemplo de componente de tipo clase que dispone de los 
+ * métodos de ciclo de vida 
+*/
+
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+
+class LifeCycleExample extends Component {
+    constructor(props) {
+        super(props);
+    }
+}
