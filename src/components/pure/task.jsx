eb790db92215646c0dce2db889606cf0b291899e
Francisco Joel Cevallos <joelcevallos363@gmail.com>
2022-11-16 21:23:36 -0500

    add bootstrap-icons

--- src/components/pure/task.jsx
+++ src/components/pure/task.jsx
@@ -5,35 +5,39 @@ import { Task } from '../../models/task.class'
 const TaskComponent = ({ task }) => {
 
     useEffect(() => {
-      console.log('Created task');
-      return() => {
-        console.log(`Task: ${task.name} is going to unmount`);
-      }
-    }, [task]) 
+        console.log('Created task');
+        return () => {
+            console.log(`Task: ${task.name} is going to unmount`);
+        }
+    }, [task])
 
     return (
-      <tr className='fw-normal'>
-        <th>
-          <span className='ms-2'>{task.name}</span>
-        </th>
-        <td className='align-middle'>
-          <span>{task.description}</span>
-        </td>
-        <td className='align-middle'>
-          {/* TODO: Sustituir por un badge */}
-          <span>{task.level}</span>
-        </td>
-        <td className='align-middle'>
-          {/* TODO: sustituir por iconos */}
-          <span>{task.completed ? 'Completed' : 'Pending'}</span>
-        </td>
-      </tr>
-      /* <div>
-        <h2>Nombre: { task.name }</h2>
-        <h3>Descripcion: { task.description }</h3>
-        <h4>Level: { task.level }</h4>
-        <h5>This task is: { task.completed ? 'COMPLETED':'PENDING'}</h5>
-      </div> */
+        <tr className='fw-normal'>
+            <th>
+                <span className='ms-2'>{task.name}</span>
+            </th>
+            <td className='align-middle'>
+                <span>{task.description}</span>
+            </td>
+            <td className='align-middle'>
+                {/* TODO: Sustituir por un badge */}
+                <span>{task.level}</span>
+            </td>
+            <td className='align-middle'>
+                {/* TODO: sustituir por iconos */}
+                { task.completed ?
+                    (<i className='bi-toggle-on' style={{color: 'green'}}></i>)
+                    : (<i className='bi-toggle-off' style={{color: 'gray'}}></i>)
+                }
+                <span>{task.completed ? 'Completed' : 'Pending'}</span>
+            </td>
+        </tr>
+        /* <div>
+          <h2>Nombre: { task.name }</h2>
+          <h3>Descripcion: { task.description }</h3>
+          <h4>Level: { task.level }</h4>
+          <h5>This task is: { task.completed ? 'COMPLETED':'PENDING'}</h5>
+        </div> */
     );
 }
 
