31cf6927e19ce74e24c49d554cb8a40f96e248e1
Francisco Joel Cevallos <joelcevallos363@gmail.com>
2022-11-23 21:20:08 -0500

    manejo de eventos

--- src/components/pure/child.jsx
+++ src/components/pure/child.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-const Child = () => {
+const Child = ({name}) => {
+
+    function pressButton() {
+        alert(`Default Text`)
+    }
+
+    function pressButtonParams(text) {
+        alert(`Text: ${text}`)
+    }
+
     return (
         <div>
-      
+            <p>Child Component</p>
+            <button onClick={() => console.log('Boton 1 pulsado')}>Boton 1</button>
+            <button onClick={() => console.log('Boton 2 pulsado')}>Boton 2</button>
         </div>
     )
 }
