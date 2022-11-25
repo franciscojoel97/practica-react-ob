0e13f72059fa3e6fcb97235b6d110f490de3a35f
Francisco Joel Cevallos <joelcevallos363@gmail.com>
2022-11-24 21:35:35 -0500

    add events

--- src/components/pure/child.jsx
+++ src/components/pure/child.jsx
@@ -15,7 +15,12 @@ const Child = ({name}) => {
             <p onMouseOver={() => console.log('On Mouse Over')}>Child Component</p>
             <button onClick={() => console.log('Boton 1 pulsado')}>Button 1</button>
             <button onClick={(pressButton) => console.log('Boton 2 pulsado')}>Button 2</button>
-            <button onClick={pressButtonParams('Hello')}>Button 3</button>
+            <button onClick={() => pressButtonParams('Hello')}>Button 3</button>
+            <input 
+                placeholder='Insert a text'
+                onFocus={() => console.log('Input focused')}
+                onChange={(e) => console.log('Input changed: ', e)}
+            />
         </div>
     )
 }
